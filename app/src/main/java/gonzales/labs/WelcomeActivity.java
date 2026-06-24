37699a6689401ef3d123da095f9febb512d72481
Samyuwel77 <smywlg1701@gmail.com>
2026-06-24 18:41:52 +0800

    Lab 2 - Edited WelcomeActivity to have realm features

--- app/src/main/java/gonzales/labs/WelcomeActivity.java
+++ app/src/main/java/gonzales/labs/WelcomeActivity.java
@@ -10,10 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// LAB 2 Addition
+import io.realm.Realm;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     TextView txtWelcome;
     SharedPreferences sharedPreferences;
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +31,24 @@ public class WelcomeActivity extends AppCompatActivity {
         });
 
         txtWelcome = findViewById(R.id.txtWelcome);
+
         sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        realm = Realm.getDefaultInstance();
 
-        String username = sharedPreferences.getString("username", "");
-        boolean rememberMe = sharedPreferences.getBoolean("rememberMe", false);
+        String savedUuid = sharedPreferences.getString("uuid", "");
 
-        if (rememberMe) {
-            txtWelcome.setText("Welcome " + username + "!!!" + " you will be remembered.");
+        User user = realm.where(User.class).equalTo("uuid", savedUuid).findFirst();
+
+        if (user != null) {
+            txtWelcome.setText("Welcome " + user.getName() + "!!!");
         } else {
-            txtWelcome.setText("Welcome !!!");
+            txtWelcome.setText("Welcome");
         }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
     }
 }
